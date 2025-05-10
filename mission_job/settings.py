b64c9570700785475de6ce9988b62c7e93c7963b
sharif-08 <otwist731@gmail.com>
2025-05-10 11:10:09 +0530

    Update settings.py

--- mission_job/settings.py
+++ mission_job/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2*-yu6p5-3+(-ra+0@a8dtz_1t_he9nyl0bp45%valn%h(+%y%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
